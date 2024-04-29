ea04aa113191a5165cbcdf3a378d1d04d5e1d37c
Joseph <josephlipe480@gmail.com>
2024-04-29 03:30:23 +0000

    Pending changes exported from your codespace

--- Chapter 2/02_07_challenge.sql
+++ Chapter 2/02_07_challenge.sql
@@ -1,1 +1,6 @@
 -- Find average daily sales
+SELECT SUM(Quantity) /
+ COUNT (DISTINCT CreationDate) as AverageDailySales
+FROM Orders
+LEFT OUTER JOIN OrderItem
+ON Orders.OrderID = OrderItem.OrderID;
--- Chapter 3/03_02b.sql
+++ Chapter 3/03_02b.sql
@@ -1,1 +1,11 @@
 -- Find infrequent customers
+SELECT 
+Customer.CustomerID,
+FirstName,
+LastName,
+COUNT(DISTINCT OrderID) as TotalOrders
+FROM Orders
+LEFT OUTER JOIN Customer
+ON Orders.CustomerID = Customer.CustomerID
+GROUP BY Customer.CustomerID
+Having COUNT(Distinct OrderID) = 1;
